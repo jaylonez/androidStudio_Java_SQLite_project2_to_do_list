fc8612ce2be47abf88a8cadcb67e5b95d66e48f4
JayloneZ <andrezyczkowski@gmail.com>
2017-11-19 14:54:51 +0000

    created taskList view which displays tasks_item in list

---
+++ app/src/main/java/com/example/user/tasklist/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.user.tasklist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TaskList taskList = new TaskList();
+
+        ArrayList<Task> tasks = taskList.getTasks();
+
+        TaskListAdapter taskAdapter = new TaskListAdapter(this, tasks);
+
+        ListView listView = findViewById(R.id.task_list);
+
+        listView.setAdapter(taskAdapter);
+    }
+
+    public void getTask(View listItemSelected) {
+
+        Task selectedTask = (Task) listItemSelected.getTag();
+
+        Toast.makeText(this, selectedTask.getName(), Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ app/src/main/java/com/example/user/tasklist/TaskListAdapter.java
@@ -0,0 +1,42 @@
+package com.example.user.tasklist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 19/11/2017.
+ */
+
+public class TaskListAdapter extends ArrayAdapter<Task> {
+
+    public TaskListAdapter(Context context, ArrayList<Task> tasks) {
+        super(context, 0, tasks);
+    }
+
+    @Override
+    public View getView(int position, View listItemView, ViewGroup parent) {
+
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.tasks_item, parent, false);
+        }
+
+        Task currentTaskItem = getItem(position);
+
+        TextView name = listItemView.findViewById(R.id.name);
+        name.setText(currentTaskItem.getName());
+
+        TextView description = listItemView.findViewById(R.id.description);
+        description.setText(currentTaskItem.getDescription());
+
+        listItemView.setTag(currentTaskItem);
+
+        return listItemView;
+
+    }
+}
