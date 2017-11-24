99d1b8c152641d566ee3bc74b876b8c20a99976a
JayloneZ <andrezyczkowski@gmail.com>
2017-11-24 12:51:08 +0000

    can now add prioritized tasks

--- app/src/main/java/com/example/user/tasklist/views/AddTaskActivity.java
+++ app/src/main/java/com/example/user/tasklist/views/AddTaskActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.example.user.tasklist.R;
@@ -23,14 +24,18 @@ public class AddTaskActivity extends LayoutActivity {
 
     }
 
+
     public void onAddTask(View view) {
         EditText new_task_name = findViewById(R.id.new_task_name);
         EditText new_task_description = findViewById(R.id.new_task_description);
+        CheckBox priority_checked = findViewById(R.id.priority_checkbox);
         TaskRepo sQLiteHelper = new TaskRepo(this);
+
 
         Task task = new Task();
         task.setName(new_task_name.getText().toString());
         task.setDescription(new_task_description.getText().toString());
+        task.setPriority(priority_checked.isChecked());
         sQLiteHelper.insertTask(task);
 
         Intent intent = new Intent(this, MainActivity.class);
